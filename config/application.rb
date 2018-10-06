ed078298f987abccd688e24762dcbb110dc10d0d
paulbaker3 <paul.baker.3@gmail.com>
2018-10-05 23:17:49 -0400

    Re-enable test generation via Rails generators
    I probably shouldn't have spent the time looking for why the model
    generator failed to create test assets. It would have been faster to
    just set things up manually. However, One of the advantages of using
    Rails is that it allows you to generate so much boilerplate code, such
    as controller tests and fixtures. And I've never seen that happen
    before, without explicitly squelching test creation from the
    commandline. When something like that shows up I want to get to the root
    issue, which in this case was commenting out the rails/test_unit/railtie
    require call.
    
    I copied in a test_helper from a boilerplate rails project and I'm back
    to a standard Rails project's testing capabilities.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_controller/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 require "bigdecimal"
 require "bigdecimal/util"
